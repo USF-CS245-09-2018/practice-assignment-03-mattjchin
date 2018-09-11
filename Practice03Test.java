0797826bce05d5df7d13553de189ce3023bfde44
Matthew Chin <mattjchin@Matthews-MacBook-Pro.local>
2018-09-11 15:55:16 -0700

    Matthew's Commit

--- Practice03Test.java
+++ Practice03Test.java
@@ -1,3 +1,5 @@
+package Lesson3;
+
 import java.util.Random;
 
 
@@ -51,18 +53,65 @@ public class Practice03Test {
 		}
 	}
 
-
+    /**
+     * find_min_iterative()
+     *
+     * This iterative function allows for the use of a variable going through each element of array.
+     * Find the index of the element with the least value out of other variables.
+     *
+     * @return the index of the element of the array with the least value
+     */
 	public int find_min_iterative () {
 		// TODO: Fill in this iterative function.
+
+        int index=0;
+        double min_value = arr[index];
+        for(int i=1; i<arr.length; i++){
+            if(arr[i] < min_value){
+                min_value = arr[i];  // Used at first to check that this index had the number with the minimum value
+                index = i;
+            }
+        }
+//        System.out.printf("Minimum Value: %.2f\n", min_value);  // Original output showed the minimum value in array
+        return index;  // Return the index of the number in array with the least value
+
 	}
 
-
+    /**
+     * find_min_recursive()
+	 *
+     * @return the index of the element number in the array with the least value
+     */
 	public int find_min_recursive () {
-		// TODO: Fill in this recursive function.
-	}
+        // TODO: Fill in this recursive function.
 
+        int index = 0;
+        int length_of_arr = arr.length;
+        return get_min_r_index(index, length_of_arr-1);
 
-	/**
+    }
+
+    /**
+     * get_min_r_index()
+     *
+	 * Called by the find_min_recursive function to conduct the recursion process for this class
+     *
+     * @param key the number that is used as the index for the recursive function (initialized at 0)
+     * @param len total number of elements in the array being used
+     * @return the index of the array with the number of the least value
+     */
+    public int get_min_r_index(int key, int len){
+
+	    if(key == len)
+	        return len;
+	    if(arr[key] < arr[len])
+	        return get_min_r_index(key, len-1);
+	    else
+	        return get_min_r_index(key+1, len);
+
+    }
+
+    /**
 	 * print_min: determines the min iteratively and recursively.
 	 * ... and prints them both.
 	 */
